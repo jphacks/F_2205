48bbf43b1dfd41cc02fbf664892ea988b460af61
mahiro72 <mahiro.yoshie@gmail.com>
2022-10-21 08:56:43 +0900

    del:

--- server/src/main.go
+++ server/src/main.go
@@ -1,33 +1,16 @@
 package main
 
 import (
-	"fmt"
-
-	// "github.com/jphacks/F_2205/server/src/infrastructure/database"
 	"github.com/jphacks/F_2205/server/src/web/router"
 )
 
 func main() {
-	fmt.Println("start")
-	// dbConn, err := database.NewConn()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer func() {
-	// 	err := dbConn.DB.Close()
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }()
 
 	// Routerの初期化
 	r := router.NewRouter()
 	r.SetMiddleware()
 
 	r.Health()
-	// r.NewRoomRouter(dbConn)
-
 	r.Ws()
 
 	// Routerの起動

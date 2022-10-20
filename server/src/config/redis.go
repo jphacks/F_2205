dfaba2bfd326b627eca4f857c05db6299b53b44b
mahiro72 <mahiro.yoshie@gmail.com>
2022-10-20 19:02:01 +0900

    fix: redis

--- server/src/config/redis.go
+++ server/src/config/redis.go
@@ -1,15 +1,14 @@
 package config
 
 import (
-	"log"
 	"os"
 )
 
 // GetRedisUrlはredisの接続先情報を取得します、もし見つからなかった場合はアプリを終了します
 func GetRedisUrl() string {
 	redisUrl := os.Getenv("REDIS_URL")
-	if redisUrl == "" {
-		log.Fatal("error: REDIS_URL not found")
-	}
+	// if redisUrl == "" {
+	// 	log.Fatal("error: REDIS_URL not found")
+	// }
 	return redisUrl
 }

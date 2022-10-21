94a49d478bf14ae172806029a8b079c5917e8d1a
KojimaNaoyuki <programkoji213@gmail.com>
2022-10-21 16:07:20 +0900

    add

--- front-end/nuxt-app/pages/room/_id.vue
+++ front-end/nuxt-app/pages/room/_id.vue
@@ -115,14 +115,30 @@ export default {
       //人数制限チェック
       setTimeout(this.roomMemberNumCheck, 5000);
       setInterval(this.roomMemberNumCheck, 60000);
+
+      //websocket roomにmemberを新規追加する
+      const data = {
+        type: 'NEW_MEMBER',
+        info: {
+          from: this.peer
+        }
+      };
+
+      this.websocketConn.send(data);
     },
 
     roomLeaving: function () {
       //ルーム退出
       this.peer.destroy();
+
+      //websocketの接続を切断(正常終了)
       this.websocketConn.close(1000, 'normal amputation websocket');
+
       alert('退出しました');
+
+      //リダイレクト
       this.$router.push('/room/prepare');
+
       this.endEstimateGaze();
       this.isVisibleSwitchButton = false;
     },
@@ -280,7 +296,7 @@ export default {
     document.querySelector('body').classList.add('modal-open');
 
     //WebSocketで接続
-    this.websocketConn = new WebSocket('wss://f-2205-server-chhumpv4gq-de.a.run.app');
+    this.websocketConn = new WebSocket('wss://f-2205-server-chhumpv4gq-de.a.run.app/ws/' + this.$route.params.id);
     this.setWebsocketEventListener(this.websocketConn);
 
     //ビデオ設定(解像度を落とす)

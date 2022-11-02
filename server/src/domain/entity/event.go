825c3ec238219b7fcf9a56316a2c9891118c504a
mahiro72 <mahiro.yoshie@gmail.com>
2022-11-02 18:56:00 +0900

    lint

--- server/src/domain/entity/event.go
+++ server/src/domain/entity/event.go
@@ -11,9 +11,9 @@ type Event struct {
 type Events map[RoomId]*Event
 
 const (
-	NewMember       EventType = "NEW_MEMBER"        // fromを新しいmemberとして追加する
-	SetFocus        EventType = "SET_FOCUS"         // from と toをつなげる
-	DelFocus        EventType = "DEL_FOCUS"         // from と toを解除する
-	DelAllFocus     EventType = "DEL_ALL_FOCUS"     // fromのすべてのconnectを削除する
-	SetScreenShot   EventType = "SET_SCREEN_SHOT"   // スクリーンショットの開始
+	NewMember     EventType = "NEW_MEMBER"      // fromを新しいmemberとして追加する
+	SetFocus      EventType = "SET_FOCUS"       // from と toをつなげる
+	DelFocus      EventType = "DEL_FOCUS"       // from と toを解除する
+	DelAllFocus   EventType = "DEL_ALL_FOCUS"   // fromのすべてのconnectを削除する
+	SetScreenShot EventType = "SET_SCREEN_SHOT" // スクリーンショットの開始
 )

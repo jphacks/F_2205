715f570f4847a16c66a6c3492c8ab69e1296a9fd
mahiro72 <mahiro.yoshie@gmail.com>
2022-11-06 04:06:55 +0900

    fix: path

--- server/src/presentation/router/room.go
+++ server/src/presentation/router/room.go
@@ -3,9 +3,9 @@ package router
 import (
 	"github.com/jphacks/F_2205/server/src/domain/service"
 	"github.com/jphacks/F_2205/server/src/infrastructure/persistance"
+	"github.com/jphacks/F_2205/server/src/presentation/handler"
+	"github.com/jphacks/F_2205/server/src/presentation/ws"
 	"github.com/jphacks/F_2205/server/src/usecase"
-	"github.com/jphacks/F_2205/server/src/web/handler"
-	"github.com/jphacks/F_2205/server/src/web/ws"
 )
 
 // InitRoomRouterはroomAPIのハンドラーを用意します

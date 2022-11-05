ccdc6cb79d1930a96c89b9a8c07d7235330611a9
Fuyan777 <fuyamiwa713@gmail.com>
2022-11-05 20:21:37 +0900

    Fix menubar to set drinking button

--- front-end/nuxt-app/components/presentational/organisms/videoState.vue
+++ front-end/nuxt-app/components/presentational/organisms/videoState.vue
@@ -44,6 +44,12 @@
           <v-icon color="white" size="42" @click.native="captureImage">mdi-monitor-screenshot</v-icon>
           <h3 class="video-state-icon-info">撮影</h3>
         </div>
+
+        <div class="video-state-icon-wrap">
+          <v-icon color="white" size="42" v-if="isEnableDrinkEstimating" @click.native="drinkEstimatingFn">mdi-glass-cocktail</v-icon>
+          <v-icon color="white" size="42" v-else @click.native="drinkEstimatingFn">mdi-glass-cocktail-off</v-icon>
+          <h3 class="video-state-icon-info">飲み検知</h3>
+        </div>
       </div>
 
       <div class="adjustWebgazerContainer">
